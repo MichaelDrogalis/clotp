2603b92217f2399b065acab631982563b7cb6456
Michael Drogalis <mjd3089@rit.edu>
2013-01-25 13:03:18 -0500

    Basic prototype.

--- src/clotp/core.clj
+++ src/clotp/core.clj
@@ -1,6 +1,28 @@
 (ns clotp.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def next-process-id (atom 0))
+(def processes (atom {}))
+
+(defn next-pid []
+  (swap! next-process-id inc))
+
+(defn spawn [f & args]
+  (let [pid (next-pid)]
+    (swap! processes #(assoc % pid (java.util.concurrent.LinkedBlockingQueue.)))
+    (future (apply f pid args))
+    pid))
+
+(defn ! [pid message]
+  (.put (get @processes pid) message))
+
+(defn ? [pid]
+  (.take (get @processes pid)))
+
+(defn my-process [self]
+  (let [message (? self)]
+    (println message)))
+
+(def pid (spawn my-process))
+(! pid "Message hereee")
+(! pid "Message hereeedsf")
+
